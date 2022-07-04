76537f95cfae0d2514390399f3867f12ec37f0f5
Pol Suar <polsuar88@gmail.com>
2022-07-04 18:15:49 -0300

    add TemperatureAndDetails and TimeAndLocation components

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeAndLocation() {
+	return (
+		<div>
+			<div className='flex items-center justify-center my-6'>
+				<p className='text-white text-xl font-extralight'>
+					Date and Time
+				</p>
+			</div>
+			<div className='flex items-center justify-center my-3'>
+				<p className='text-white text-3xl font-medium'>
+					City
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default TimeAndLocation
