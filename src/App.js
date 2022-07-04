a17a86d12e2c3c53a94180b8823362babf7cffa7
Pol Suar <polsuar88@gmail.com>
2022-07-04 13:00:50 -0300

    add Unicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className = "text-2xl font-bold text-red-500">Hello World</h1>
+      <UilReact size="140" color="#61DAFB" />
     </div>
   );
 }
